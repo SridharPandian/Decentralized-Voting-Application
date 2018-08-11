7dca255edeb2b8219c8d07c7e85cca6e5b2d3af0
SridharPandian <sripan172@gmail.com>
2018-08-11 12:49:10 +0530

    Added individual candidate options in Election.sol

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,24 @@
 pragma solidity ^0.4.24;
 
 contract Election {
-    string public candidate;
-
-    constructor() public {
-        candidate = "Candidate 1";
+    
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
     }
+
+    uint public candidateCount;
+    mapping(uint => Candidate) public Candidates;
+    
+    constructor() public {
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate(string _name) private {
+        candidateCount++;
+        Candidates[candidateCount] = Candidate(candidateCount, _name, 0);
+    }
+
 }
